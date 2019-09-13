3cadb08406abb452624277229b901ff7fefe1e2b
JEFFREY <hoipck@gmail.com>
2019-09-13 03:49:16 +0000

    update

--- hidtl.py
+++ hidtl.py
@@ -34,20 +34,20 @@ def readCommand(argv):
     parser.add_option('-i', '--inputFile', dest='inputFile',
                       help='the path to an input file of a species tree', metavar='INPUT_FILE')
     parser.add_option('-c', '--coalescentArgs', dest='coalescentArgs',
-                      help=default('the parameters of the gamma distribution for coalescent, \
-                      e.g., "shape=val1,scale=val2", or "const=val" if constant applies'),
+                      help=default('the parameters of the gamma distribution for coalescent, '
+                      'e.g., "shape=val1,scale=val2", or "const=val" if constant applies'),
                       default='shape=1,scale=1')
     parser.add_option('-d', '--duplicationArgs', dest='duplicationArgs',
-                      help=default('the parameters of the gamma distribution for duplication event, \
-                      e.g., "shape=val1,scale=val2", or "const=val" if constant applies'),
+                      help=default('the parameters of the gamma distribution for duplication event, '
+                      'e.g., "shape=val1,scale=val2", or "const=val" if constant applies'),
                       default='shape=1,scale=1')
     parser.add_option('-t', '--transferArgs', dest='transferArgs',
-                      help=default('the parameters of the gamma distribution for transfer event, \
-                      e.g., "shape=val1,scale=val2", or "const=val" if constant applies'),
+                      help=default('the parameters of the gamma distribution for transfer event, '
+                      'e.g., "shape=val1,scale=val2", or "const=val" if constant applies'),
                       default='shape=1,scale=1')
     parser.add_option('-l', '--lossArgs', dest='lossArgs',
-                      help=default('the parameters of the gamma distribution for loss event, \
-                      e.g., "shape=val1,scale=val2", or "const=val" if constant applies'),
+                      help=default('the parameters of the gamma distribution for loss event, '
+                      'e.g., "shape=val1,scale=val2", or "const=val" if constant applies'),
                       default='shape=1,scale=1') 
     parser.add_option('-h', '--hemiplasy', type='int', dest='hemiplasy',
                       help=default('hemiplasy option, 0 or 1'), metavar='HEMIPLASY',
